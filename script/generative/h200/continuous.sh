82c9754e4230fec1db0ffedc045d805dfdea1ca6
106 <Segmentation-fault@yandex.ua>
2020-06-05 13:14:15 +0900

    人工データに対する学習設定・スクリプトを追加

---
+++ script/generative/h200/continuous.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+  echo "requires 1 arguments." 1>&2
+  exit 1
+fi
+
+DIR="./results/generative/"`date +%Y-%m-%d_%H-%M-%S`"_h200_continuous/"
+mkdir -p $DIR
+
+for i in `seq $1`
+do
+    ./train_generative.py ./config/generative/h200/continuous.json 3000 -d $DIR
+done
