eec30f78057fb6ba8025e1e28dd882751b3d3ef9
106 <Segmentation-fault@yandex.ua>
2020-06-06 14:27:12 +0900

    スパースパラメータの分布用確認用の実験設定を追加

---
+++ script/histogram/h2000.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+  echo "requires 1 arguments." 1>&2
+  exit 1
+fi
+
+DIR="./results/histogram/"`date +%Y-%m-%d_%H-%M-%S`"_h2000_continuous_sparse/"
+mkdir -p $DIR
+
+for i in `seq $1`
+do
+    ./train_generative.py ./config/histogram/h2000.json 3000 -d $DIR
+done
