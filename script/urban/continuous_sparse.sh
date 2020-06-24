d1c4caba62123611087e24212a5c66975affc9f8
106 <Segmentation-fault@yandex.ua>
2020-06-24 16:44:36 +0900

    kaist urban datasetの実験設定を追加

---
+++ script/urban/continuous_sparse.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+  echo "requires 1 arguments." 1>&2
+  exit 1
+fi
+
+DIR="./results/urban/"`date +%Y-%m-%d_%H-%M-%S`"_h300_continuous_sparse/"
+mkdir -p $DIR
+
+for i in `seq $1`
+do
+    ./train_urban.py ./config/urban/continuous_sparse.json 1000 -d $DIR
+done
---
+++ train_urban.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import json
+import argparse
+import numpy as np
+import tensorflow as tf
+import datetime
+import os
+from DRBM import DRBM
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import fetch_covtype
+from mltools import LearningLog
+
+parser = argparse.ArgumentParser("DRBM learning script.", add_help=False)
+parser.add_argument("learning_config", action="store", type=str, help="path of learning configuration file.")
+parser.add_argument("learning_epoch", action="store", type=int, help="numbers of epochs.")
+parser.add_argument("-d", "--output_directory", action="store", type=str, default="./results/", help="directory to output parameter & log")
+parser.add_argument("-s", "--filename_suffix", action="store", type=str, default=None, help="filename suffix")
+args = parser.parse_args()
+
+config = json.load(open(args.learning_config, "r"))
+ll = LearningLog(config)
+
+y, x = np.split(np.loadtxt("./data.txt", delimiter=","), [1], 1)
+
+x_mean = np.mean(x, 0)
+x_std = np.std(x, 0)
+x = (x - x_mean) / x_std
+
+y = to_categorical(y)
+
+dtype = config["dtype"]
+x = x.astype(dtype)
+y = y.astype(dtype)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, shuffle=False)
+
+if "learning_data_limit" in config:
+    idx = np.random.choice(np.arange(0, len(x_train)), size=len(x_train), replace=False)
+    x_train = x_train[idx[0:config["learning_data_limit"]]]
+    y_train = y_train[idx[0:config["learning_data_limit"]]]
+
+train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(100)
+
+optimizer = tf.keras.optimizers.Adamax(learning_rate=0.002, epsilon=1e-8)
+drbm = DRBM(*config["training-layers"], **config["training-args"], dtype=dtype)
+drbm.fit_categorical(args.learning_epoch, len(x_train), config["minibatch-size"], optimizer, train_ds, test_ds, ll)
+
+now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+filename = [
+    now,
+    "mnist",
+    "h"+str(config["training-layers"][1]),
+    config["training-args"]["activation"]
+]
+if args.filename_suffix is not None:
+    filename.append(args.filename_suffix)
+filename.append("%s.json")
+filename = "_".join(filename)
+
+filepath = os.path.join(args.output_directory, filename%"log")
+ll.save(filepath)
