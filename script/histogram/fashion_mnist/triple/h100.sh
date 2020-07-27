85724fbdb05192fa0c582d4df4be9d17a2046dbd
106 <Segmentation-fault@yandex.ua>
2020-07-27 18:40:17 +0900

    抑制度計算用スクリプトのフォルダ構造の整頓とfashion-MNISTの追加

---
+++ script/histogram/fashion_mnist/triple/h100.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+  echo "requires 1 arguments." 1>&2
+  exit 1
+fi
+
+DIR="./results/histogram_fashion_mnist/"`date +%Y-%m-%d_%H-%M-%S`"_h100_triple_sparse/"
+mkdir -p $DIR
+
+for i in `seq $1`
+do
+    ./train_fashion_mnist.py ./config/histogram/triple_mnist/h100.json 100 -d $DIR -p
+done
